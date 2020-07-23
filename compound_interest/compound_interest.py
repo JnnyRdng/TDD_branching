32a6b978ddc5b4e8cce3082f3b9a9901f8735963
Jonny Reading <jonnyreading@gmail.com>
2020-07-23 21:57:42 +0100

    Added years to init, test passes

--- compound_interest/compound_interest.py
+++ compound_interest/compound_interest.py
@@ -1,4 +1,5 @@
 class CompoundInterest:
-    def __init__(self, principal, percent):
+    def __init__(self, principal, percent, years):
         self.principal = principal
         self.percent = percent
+        self.years = years
